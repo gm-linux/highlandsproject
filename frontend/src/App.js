9c9ceecd24f5eb6071fb6a987108aa5d63e5c7b4
gm-linux <justmikey2002@gmail.com>
2025-04-02 15:52:24 -0400

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
-import ProjectDetails from "./pages/ProjectDetails";
 
 function App() {
   return (
@@ -9,8 +8,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/projects/:id" element={<ProjectDetails />} />
       </Routes>
     </Router>
   );
